3f546f2301a43623c6180baf7d4bcb023be383fa
Emir Dedic <dedic.emir17@gmail.com>
2022-04-14 17:07:42 +0200

    main

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,3 +1,37 @@
 // Only change code below this line
+function* myGenerator() {
+    yield* insideGenerator1();
+    yield* insideGenerator2();
+    yield* insideGenerator3();
+}
+
+function* insideGenerator1() {
+    for (let x = 0; x < 6; x++) {
+        yield x; 
+    }
+}
+
+function* insideGenerator2() {
+    for (let x = 10; x < 16; x++) {
+        yield x;
+    }
+}
+
+function* insideGenerator3() {
+    for (let x = 6; x < 10; x++) {
+        yield x;
+    }
+}
+
+const iterator = myGenerator();
+iterator.next().value;
+
+const fifteenArray = [];
+for (let i = 0; i < 16; i++) {
+    let iter = iterator.next();
+    fifteenArray[i] = iter.value + (iter.done ? "!" : "#, ");
+}
+
+console.log(fifteenArray);
 // Only change code above this line
 module.exports = { fifteenArray, myGenerator };
--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,3 +1,30 @@
 // Only change code below this line
+let shoppingList = new Map([
+    ["Banana", 3],
+    ["Pineapple", 5],
+    ["Pear", 2],
+    ["Carrot", 10],
+    ["Apple", 1.5]
+]);
+
+let arrKey = [];
+
+arrKey = [...shoppingList];
+arrKey.forEach((key) => {
+    console.log(`groceries: ${key[0]}`);
+});
+// console.log(arrKey instanceof Array);
+
+let arrValue = [];
+
+arrValue = [...shoppingList];
+arrValue.forEach((val) => {
+    console.log(`amount: ${val[1]}`);
+});
+// console.log(arrKey instanceof Array)
+
+for (let entry of shoppingList.entries()) {
+    console.log(`['${entry[0]}', ${entry[1]}]`);
+}
 // Only change code above this line
 module.exports = { shoppingList, arrKey, arrValue };
